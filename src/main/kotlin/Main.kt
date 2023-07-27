a975f12fa6ad0f5ef52dc0d10030aa1bc59e5c1b
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-07-27 10:25:28 +0200

    Start Terminal application through kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-
-    test(1);
+fun main() {
+    startTerminal()
 }
 
-fun test(a: Int){
-    println("tests are important");
-    if(a == 1){
-        println("abcd");
-
-
-    }
+fun startTerminal() {
+    Runtime.getRuntime().exec("open /System/Applications/Utilities/Terminal.app")
 }
