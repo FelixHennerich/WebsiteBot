8f8485490eb9b8a6d415e3712db2a3c46bfe380f
Felix Hennerich <66116721+BlueNitrox@users.noreply.github.com>
2023-07-29 20:11:53 +0200

    Basic understanding of methods and classes in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import okhttp3.Response
 
 fun main() {
     htmlRequest()
+    TestClass().testfuncation()
 }
 
 fun startTerminal() {
---
+++ src/main/kotlin/TestClass.kt
@@ -0,0 +1,6 @@
+class TestClass {
+
+    fun testfuncation(){
+        println("This is a test")
+    }
+}
